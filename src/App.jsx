1a9c498890cc3d11ec9b998c7e94885ca0b935f0
quynIT <trungquyen2902@gmail.com>
2024-07-23 18:14:35 +0700

    App main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Pages from './Components/Pages/Pages'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 
