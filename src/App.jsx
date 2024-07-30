210ed7539f2242dc8329e7c0dada62eeb45140df
quynIT <trungquyen2902@gmail.com>
2024-07-30 17:01:40 +0700

    edit app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import { NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <h1 className="App">
           <Navbar></Navbar>
+          <NavLink></NavLink>
     </h1>
   );
 }
