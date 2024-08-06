0beaa8e61e6f5ead438dfc26dc6bf7774a83f4fb
quynIT <trungquyen2902@gmail.com>
2024-08-06 08:51:11 +0700

    update route

--- src/Components/Pages/Pages.jsx
+++ src/Components/Pages/Pages.jsx
@@ -11,8 +11,8 @@ const Pages = () => {
     <div>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/Login" element={<Login></Login>}></Route>
+        <Route path="/Register" element={<Register></Register>}></Route>
         <Route path="reset" element={<Reset></Reset>}></Route>
         <Route
           path="/profile/:id"
