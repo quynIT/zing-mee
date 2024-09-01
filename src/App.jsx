ec2e54cb986144f5f8609ee31780e1003a8d35cf
quynIT <trungquyen2902@gmail.com>
2024-09-01 22:04:54 +0700

    App Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import Pages from "./Components/Pages/Pages";
 import { BrowserRouter } from "react-router-dom";
-import AppContext from "./Components/AppContext/AppContext";
-
 function App() {
   return (
     <h1 className="App">
